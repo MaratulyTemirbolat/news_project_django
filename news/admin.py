11dec4618f4665971ea1fadf4c4131f645ff059a
Temirbolat Maratuly <snake2009003@mail.ru>
2022-03-07 00:28:03 +0600

    Added Trial Mixin, Pagination for function view (trial) and for class Base (HomeNews, Category). Corrected in some places PEP-8

--- news/admin.py
+++ news/admin.py
@@ -1,19 +1,40 @@
+from django.utils.safestring import mark_safe
 from django.contrib import admin
 
 from .models import News
 from .models import Category
 
+
 class NewsAdmin(admin.ModelAdmin):
-    list_display = ('id','title','created_at','updated_at','is_published','category')
-    list_display_links = ('id','title')
-    search_fields = ('title','content')
+    list_display = ('id', 'title', 'created_at', 'updated_at',
+                    'is_published', 'category', 'get_photo')
+    list_display_links = ('id', 'title')
+    search_fields = ('title', 'content')
     list_editable = ('is_published',)
-    list_filter = ('is_published','category')
+    list_filter = ('is_published', 'category')
+    fields = ('title', 'category', 'content',
+              'photo', 'created_at', 'views_number', 'updated_at',
+              'is_published', 'get_photo')
+    # Поля, которые нужны для вывода внутри самой новости ('fields')
+    readonly_fields = ('get_photo', 'views_number', 'created_at', 'updated_at')
+    save_on_top = True
+
+    def get_photo(self, obj):
+        if obj.photo:
+            return mark_safe(f'<img src="{obj.photo.url}" width="150">')
+        return '-'
+    get_photo.short_description = 'Миниатюра'
+    # С помощью этого сво-ва у методов можно дать описание методов
+
 
 class CategoryAdmin(admin.ModelAdmin):
-    list_display = ('id','title')
-    list_display_links = ('id','title')
+    list_display = ('id', 'title')
+    list_display_links = ('id', 'title')
     search_fields = ('title',)
 
-admin.site.register(News,NewsAdmin)
-admin.site.register(Category,CategoryAdmin)
+
+admin.site.register(News, NewsAdmin)
+admin.site.register(Category, CategoryAdmin)
+
+admin.site.site_title = 'Управление новостями'
+admin.site.site_header = 'Управление новостями'
--- news/views.py
+++ news/views.py
@@ -1,36 +1,72 @@
-from hashlib import new
-from unicodedata import category
-from django import template
+# from hashlib import new
+# from unicodedata import category
+# from django import template
+# from django.shortcuts import render
+# from matplotlib.style import context
+# from sympy import N, Ne
+# from django.shortcuts import get_object_or_404
+# from django.shortcuts import redirect
 from django.shortcuts import render
-from matplotlib.style import context
-from sympy import N, Ne
-from django.shortcuts import get_object_or_404
-from django.shortcuts import redirect
-from django.views.generic import ListView # Данный класс предназначен для работы со списками. Задача его, вернуть список. Помагает вернуть список объектов для какой-то страницы
-from django.views.generic import DetailView # Данный класс предназнен для конкретной новости (Экземпляра объекта)
-from django.views.generic import CreateView # Данный класс предназначен для создание новости (Экземпляра объекат), работа с Формой
+from django.views.generic import ListView
+# Данный класс предназначен для работы со списками.
+# Задача его, вернуть список.
+# Помогает вернуть список объектов для какой-то страницы
+from django.views.generic import DetailView
+# Данный класс предназнен для конкретной новости (Экземпляра объекта)
+from django.views.generic import CreateView
+# Данный класс предназначен для создание новости
+# (Экземпляра объекат), работа с Формой
+from django.contrib.auth.mixins import LoginRequiredMixin
+# Используем для тех классов, для которых надо ограничить доступ
+from django.core.paginator import Paginator
 
 from .models import News
 from .models import Category
 
 from .forms import NewsForm
 
-from django.urls import reverse_lazy # Делает тоже самое, что и reverse, но он срабатывает только тогда, когда до него дойдет очередь. Т.е когда все будет только работать, все маршруты подгрузятся, то reverse_lazy построит ссылку
+# from django.urls import reverse_lazy 
+# # Делает тоже самое, что и reverse, но он срабатывает 
+# только тогда, когда до него дойдет очередь. 
+# Т.е когда все будет только работать, все маршруты подгрузятся, 
+# то reverse_lazy построит ссылку
+
+
+def test(request):
+    objects = ['john1', 'paul2',
+               'george3', 'ringo4',
+               'john5', 'paul6',
+               'george7', ]
+    paginator = Paginator(objects, 2)
+    page_num = request.GET.get('page', 1)
+    # Если нет page в url адресе, то будет присвоена 1 по умолчанию
+    page_objects = paginator.get_page(page_num)
+    # Получем объекты заданной страницы
+    return render(request, 'news/test.html', {'page_obj': page_objects})
+
 
 class HomeNews(ListView):
     # Есть аттрибуты, которые мы должны переобределить.
-    model = News # Модель, из который мы хотим получить список.Аналог news = News.objects.all()
-    template_name = 'news/home_news_list.html' # Путь к нужному нам шаблону
+    model = News
+    # Модель, из который мы хотим получить список.
+    # Аналог news = News.objects.all()
+    template_name = 'news/home_news_list.html'
+    # Путь к нужному нам шаблону
     context_object_name = 'news'
-    # extra_context = {'title':'Список новостей'} # Лучше использовать для статичных данных. 
+    # extra_context = {'title':'Список новостей'}
+    # # Лучше использовать для статичных данных.
+    paginate_by = 2
 
-    def get_context_data(self,**kwargs):
-        context = super().get_context_data(**kwargs) # В переменную 'context' записали все то, что было там до этого (Все данные)
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # В переменную 'context' записали все то,
+        # что было там до этого (Все данные)
         context['title'] = 'Главная страница'
         return context
-    
-    def get_queryset(self): # Получаем отфильтрованный набор объектов
-        return News.objects.filter(is_published=True).select_related('category')
+
+    def get_queryset(self):  # Получаем отфильтрованный набор объектов
+        return News.objects.\
+            filter(is_published=True).select_related('category')
 
 # def index(request):
 #     #print(dir(request))
@@ -42,20 +78,29 @@ class HomeNews(ListView):
         
 #     return render(request,'news/index.html',context)
 
+
 class NewsByCategory(ListView):
     model = News
     template_name = 'news/home_news_list.html'
     context_object_name = 'news'
-    allow_empty = False # Когда список пуст, т.е записей нет, то смысла показывать его нет.
-    # queryset = News.objects.select_related('category') Альтернатива методу get_queryset(self)
+    allow_empty = False
+    # Когда список пуст, т.е записей нет, то смысла показывать его нет.
+    # queryset = News.objects.select_related('category')
+    #  Альтернатива методу get_queryset(self)
+    paginate_by = 2
 
-    def get_context_data(self,**kwargs):
-        context = super().get_context_data(**kwargs) # В переменную 'context' записали все то, что было там до этого (Все данные)
-        context['title'] = Category.objects.get(pk = self.kwargs['category_id'])
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        # В переменную 'context' записали все то,
+        # что было там до этого (Все данные)
+        context['title'] = Category.objects.get(pk=self.kwargs['category_id'])
         return context
 
-    def get_queryset(self): # в self можно получить параметры URL
-        return News.objects.filter(category_id = self.kwargs['category_id'],is_published = True).select_related('category')
+    def get_queryset(self):
+        # в self можно получить параметры URL
+        return News.objects.\
+            filter(category_id=self.kwargs['category_id'],
+                   is_published=True).select_related('category')
 
 # def get_category(request,category_id):
 #     news = News.objects.filter(category_id = category_id)
@@ -66,6 +111,7 @@ class NewsByCategory(ListView):
 #         'category':category
 #     }
 #     return render(request,'news/category.html',context)
+
 
 class ViewNews(DetailView):
     model = News
@@ -79,22 +125,35 @@ class ViewNews(DetailView):
 #     return render(request,'news/view_news.html',{'news_item':news_item})
 
 
-class CreateNews(CreateView):
+class CreateNews(LoginRequiredMixin, CreateView):
     # Нужно связать данный класс с формой
     form_class = NewsForm  # Указываем нужный нам класс формы
     template_name = 'news/add_news.html'
-    # success_url = reverse_lazy('home') #Если не написать, то по умолчанию будет вызываться get_absolute_url той модели, которая подвязана
+    # success_url = reverse_lazy('home') #Если не написать,
+    # то по умолчанию будет вызываться get_absolute_url той модели,
+    # которая подвязана
+    # login_url = '/admin/'
+    raise_exception = True 
 
 
 # Работа с Формами, несвязанными с моделями
 # def add_news(request):
 #     if request.method == 'POST':
-#         form = NewsForm(request.POST) # Форма, связанная с данными. Все данные, введенные пользователем, не удалятся после submit.
-#         if form.is_valid(): # Проверка: прошли данные валидацию, которые были настроены в модуле 'forms.py' для каждого поля
-#             # print(form.cleaned_data)  # После того, как форма прошла Валидацию, то все данные, которые были введены, попадают в словарь 'cleaned_data'
-#             #news = News.objects.create(**form.cleaned_data) # Две звездочки автоматически распакуют данный словарь и присвоит ключам автоматически соответствующие значения. Create также возвращает полученный объект
+#         form = NewsForm(request.POST) # Форма, связанная с данными.
+# Все данные, введенные пользователем, не удалятся после submit.
+#         if form.is_valid(): # Проверка: прошли данные валидацию,
+# которые были настроены в модуле 'forms.py' для каждого поля
+#             # print(form.cleaned_data)  # После того, как форма прошла
+# Валидацию, то все данные, которые были введены,
+# попадают в словарь 'cleaned_data'
+#             #news = News.objects.create(**form.cleaned_data)
+# # Две звездочки автоматически распакуют данный словарь и
+# присвоит ключам автоматически соответствующие значения.
+# Create также возвращает полученный объект
 #             news = form.save()
-#             return redirect(news) # Универсальный метод redirect. Можно указывать name из url куда мы хотим пойти или как видно сам экземпляр класса. 
+#             return redirect(news) # Универсальный метод redirect.
+# Можно указывать name из url куда
+# мы хотим пойти или как видно сам экземпляр класса.
 #     else:
 #         form = NewsForm() # Форма, несвязанная с данными
 #     return render(request,'news/add_news.html',{'form':form})
